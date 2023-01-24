d24e0ecd77e2aabf16f848128d037b50fde0f11b
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-24 12:26:57 -0300

    Realizado a criação dos components Progesso e Tentativas

--- projetos/app1/src/app/app.module.ts
+++ projetos/app1/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule,
