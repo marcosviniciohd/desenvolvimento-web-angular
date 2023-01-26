24e0c469b41848e95077d203fc8445d1a2ac2224
Marcos <marcosviniciohd@hotmail.com>
2023-01-26 17:04:08 -0300

    Alterado componente tentativas

--- projetos/app2/src/app/tentativas/tentativas.component.ts
+++ projetos/app2/src/app/tentativas/tentativas.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class TentativasComponent {
 
+  public coracaoVazio: string = '/assets/coracao_vazio.png';
+  public coracaoCheio: string = '/assets/coracao_cheio.png';
+
 }
--- projetos/app2/src/app/topo/topo.component.ts
+++ projetos/app2/src/app/topo/topo.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./topo.component.css']
 })
 export class TopoComponent {
-
+ titulo: string = 'Apredendo Inglês'
 }
