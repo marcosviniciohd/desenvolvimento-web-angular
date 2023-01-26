64ec87c33970c3c9bb3ba991fb7c3cb7631b3214
Marcos <marcosviniciohd@hotmail.com>
2023-01-26 17:40:36 -0300

    event binding na prática

--- projetos/app2/src/app/painel/painel.component.ts
+++ projetos/app2/src/app/painel/painel.component.ts
@@ -9,10 +9,17 @@ import { FRASES } from './frases-mock';
 })
 export class PainelComponent {
 
+  public i: number = 0;
+
+  public instrucao: string = 'Traduza a frase';
   public frases: Frase[] = FRASES;
 
   constructor(){
     console.log(this.frases);
+  }
+
+  public atualizaResposta(): void {
+    console.log('Teste ' + this.i++);
   }
 
 }
