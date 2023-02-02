888513dbf13080fd844400d2866e866515b08f2e
Marcos <marcosviniciohd@hotmail.com>
2023-02-02 18:04:35 -0300

    Injetando o serviço de ofertas no componente Home

--- projetos/passaro-urbano/src/app/home/home.component.ts
+++ projetos/passaro-urbano/src/app/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { OfertasService } from '../ofertas.services';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent {
+
+  constructor(private ofertasService: OfertasService){
+
+  }
+
+  ngOnInit() {
+      console.log(this.ofertasService.getOfertas());
+  }
 
 }
