d07d1c5f008c2cb3d3ab701b0b9fa71a564547df
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-06-01 15:54:06 -0300

    feat: exibindo corações com ngFor

--- app1/src/app/tentativas/tentativas.component.ts
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Coracao } from '../shared/model/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
@@ -10,8 +11,16 @@ export class TentativasComponent implements OnInit {
   public coracaoVazio: string = '/assets/coracao_vazio.png';
   public coracaoCheio: string = '/assets/coracao_cheio.png';
 
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true),
+  ];
 
-  constructor() { }
+
+  constructor() {
+    console.log(this.coracoes);
+  }
 
   ngOnInit(): void {
   }
